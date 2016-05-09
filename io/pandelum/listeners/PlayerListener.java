9edc9f2e1bbe5d215d337431343471d2943671a7
David Bell <dave@evad.info>
2016-05-09 14:28:01 +0100

    Permissions are rebuilt for a player when they change world

--- io/pandelum/listeners/PlayerListener.java
+++ io/pandelum/listeners/PlayerListener.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -158,5 +159,13 @@ public class PlayerListener implements Listener
 		
 		// Remove the permissions for this player 'cos they're going offline
 		core.deletePlayerPermissions(player);
+	}
+	
+	// When a player changes world we must rebuild their permissions 
+	// to reflect the permissions they should have in the new world
+	@EventHandler
+	public void onPlayerSwitchWorld(PlayerChangedWorldEvent event)
+	{
+		core.rebuildPlayerPermissions(event.getPlayer(), event.getPlayer().getWorld());
 	}
 }
